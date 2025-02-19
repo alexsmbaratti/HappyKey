38b0980c0be19acf0c626dbf54b563d24ba152fd
Alex Baratti <32379831+alexsmbaratti@users.noreply.github.com>
2025-02-18 21:23:24 -0800

    Converting to SwiftUI

---
+++ HostingApp/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  HostingApp
+//
+//  Created by Alex Baratti on 2/18/25.
+//  Copyright © 2025 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("Test")
+            Spacer()
+        }
+//        .padding()
+//        .background(
+//            LinearGradient(
+//                gradient: Gradient(colors: [Color("LightBlue"), Color("DarkBlue")]),
+//                startPoint: .top,
+//                endPoint: .bottom
+//            )
+//        )
+    }
+}
+
+#Preview {
+    ContentView()
+}
